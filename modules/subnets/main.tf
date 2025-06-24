71dfb9aa975a087eafe81d061e7b65e6a09ddbab
venkatmandru333 <venkatmandru333@gmail.com>
2025-06-24 13:55:02 +0530

    Update main.tf

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -4,14 +4,12 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
-
   tags = {
     Name        = "${var.environment}-public-${count.index + 1}"
     Environment = var.environment
     Type        = "public"
   }
 }
-
 resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs)
   vpc_id            = var.vpc_id
@@ -24,7 +22,6 @@ resource "aws_subnet" "private" {
     Type        = "private"
   }
 }
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = var.vpc_id
 
@@ -33,7 +30,28 @@ resource "aws_internet_gateway" "igw" {
     Environment = var.environment
   }
 }
+resource "aws_eip" "nat" {
+  count = length(var.azs)
 
+  vpc = true
+
+  tags = {
+    Name        = "${var.environment}-nat-eip-${count.index + 1}"
+    Environment = var.environment
+  }
+}
+resource "aws_nat_gateway" "nat" {
+  count         = length(var.azs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name        = "${var.environment}-nat-${count.index + 1}"
+    Environment = var.environment
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
 resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
 
@@ -48,23 +66,29 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Public Route Table Association
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
-
 resource "aws_route_table" "private" {
+  count  = length(var.azs)
   vpc_id = var.vpc_id
 
   tags = {
-    Name        = "${var.environment}-private-rt"
+    Name        = "${var.environment}-private-rt-${count.index + 1}"
     Environment = var.environment
   }
 }
-
+resource "aws_route" "private_nat" {
+  count                  = length(var.azs)
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+}
 resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
